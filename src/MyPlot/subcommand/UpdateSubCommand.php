4513baf228e2a0455e73d6980c3705969f81bf06
Blumieare Exxarion <Apluscomics@live.com>
2016-04-09 16:52:04 -0400

    Create UpdateSubCommand.php

---
+++ src/MyPlot/subcommand/UpdateSubCommand.php
@@ -0,0 +1,26 @@
+<?php
+namespace MyPlot\subcommand;
+
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class UpdateSubCommand extends SubCommand
+{
+    public function canUse(CommandSender $sender) {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.command.update");
+    }
+
+    public function execute(CommandSender $sender, array $args) {
+    $this->getLogger()->info(TextFormat::GREEN."Checking for any updates...");
+				$lst = Utils::getURL("https://raw.githubusercontent.com/wiez/MyPlot/master/plugin.yml");
+				
+				$dsc = \yaml_parse($lst);
+				
+				$description = $this->getDescription();
+				if($description->getVersion() !== $dsc['version']){
+					$this->getLogger()->info(TextFormat::YELLOW."MyPlot v".$dsc["version"]." has been released. Please download the latest version here:\n".TextFormat::GOLD."http://github.com/wiez/MyPlot/releases");
+				}else{
+					$this->getLogger()->info(TextFormat::GREEN."Your version of MyPlot is up-to-date! No update is required.");
+				}
+			}
