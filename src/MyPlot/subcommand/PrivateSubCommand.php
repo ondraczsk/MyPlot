199b97921ea550effe38a59be1ce604a801618fe
Blumieare Exxarion <Apluscomics@live.com>
2016-04-09 17:47:34 -0400

    Create PrivateSubCommand.php

---
+++ src/MyPlot/subcommand/PrivateSubCommand.php
@@ -0,0 +1,38 @@
+<?php
+namespace MyPlot\subcommand;
+
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
+
+class PrivateSubCommand extends SubCommand
+{
+    public function canUse(CommandSender $sender) {
+        return ($sender instanceof Player) and $sender->hasPermission("myplot.command.private");
+    }
+
+    public function execute(CommandSender $sender, array $args) {
+    
+        $player = $sender->getServer()->getPlayer($sender->getName());
+        $plot = $this->getPlugin()->getPlotByPosition($player->getPosition());
+        if ($plot === null) {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("notinplot"));
+            return true;
+        }
+        if ($plot->owner !== $sender->getName() and !$sender->hasPermission("myplot.admin.private")) {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("notowner"));
+            return true;
+        }
+        if (!$plot->isPrivate($plot)) {
+            $sender->sendMessage($this->translateString("private.already"));
+            return true;
+        }
+        if ($this->getPlugin()->getProvider()->savePlot($plot)) {
+            $sender->sendMessage($this->translateString("private.success"));
+            $plot->plotPrivate = "True";
+        } else {
+            $sender->sendMessage(TextFormat::RED . $this->translateString("error"));
+        }
+        return true;
+    }
+}
